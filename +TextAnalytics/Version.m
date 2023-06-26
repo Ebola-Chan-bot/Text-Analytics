9d506b453c8ae5965be2a464ccf7b1378e2b5faf
埃博拉酱 <vhtmfu@outlook.com>
2023-06-26 15:13:56 +0800

    要求 MATLAB R2023b 和MATLAB扩展

--- +TextAnalytics/Version.m
+++ +TextAnalytics/Version.m
@@ -1,6 +1,6 @@
 function V = Version
-V.Me='v1.0.2';
-V.MATLAB='R2021a';
+V.Me='v1.0.3';
+V.MATLAB='R2023b';
 persistent NewVersion
 if isempty(NewVersion)
 	NewVersion=TextAnalytics.CheckUpdateFromGitHub('https://github.com/Silver-Fang/Text-Analytics/releases','埃博拉酱的文本分析工具箱',V.Me);
---
+++ TextAnalyticsException.m
@@ -0,0 +1,5 @@
+classdef TextAnalyticsException<MATLAB.Lang.IEnumerableException
+	enumeration
+		Thread_parallelism_not_supported
+	end
+end
