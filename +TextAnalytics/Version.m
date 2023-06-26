06e49edb38dadac5ec5ba4e72fa6734542ab4020
埃博拉酱 <vhtmfu@outlook.com>
2023-06-26 15:16:34 +0800

    提示：不能在线程并行池中使用

--- +TextAnalytics/Version.m
+++ +TextAnalytics/Version.m
@@ -1,6 +1,6 @@
 function V = Version
 V.Me='v1.0.3';
-V.MATLAB='R2023b';
+V.MATLAB='R2021a';
 persistent NewVersion
 if isempty(NewVersion)
 	NewVersion=TextAnalytics.CheckUpdateFromGitHub('https://github.com/Silver-Fang/Text-Analytics/releases','埃博拉酱的文本分析工具箱',V.Me);
