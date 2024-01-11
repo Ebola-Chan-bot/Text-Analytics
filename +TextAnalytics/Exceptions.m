ad20a69894ee0370db0cdd1171483b1fda959b62
埃博拉酱 <vhtmfu@outlook.com>
2024-01-11 11:15:46 +0800

    v1.0.3

--- +TextAnalytics/Exceptions.m
+++ +TextAnalytics/Exceptions.m
@@ -1,4 +1,4 @@
-classdef TextAnalyticsException<MATLAB.Lang.IEnumerableException
+classdef Exceptions<MATLAB.Lang.IEnumerableException
 	enumeration
 		Thread_parallelism_not_supported
 		Latest_release_not_found
