9f67b205d12d9a5799ea99bde02ba48654f89f60
埃博拉酱 <vhtmfu@outlook.com>
2025-03-12 10:07:24 +0800

    v1.0.4

--- +TextAnalytics/Version.m
+++ +TextAnalytics/Version.m
@@ -1,8 +1,8 @@
 function V = Version
-V.Me='v1.0.3';
+V.Me='v1.0.4';
 V.MATLAB='R2023b';
 V.MatlabExtension='16.0.0';
 persistent NewVersion
 if isempty(NewVersion)
-	NewVersion=TextAnalytics.CheckUpdateFromGitHub('https://github.com/Silver-Fang/Text-Analytics/releases','埃博拉酱的文本分析工具箱',V.Me);
+	NewVersion=TextAnalytics.CheckUpdateFromGitHub('https://github.com/Ebola-Chan-bot/Text-Analytics/releases','埃博拉酱的文本分析工具箱',V.Me);
 end
